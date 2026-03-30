6c995b8033ffc6e3526182fd421a6118b6028920
GeogeoB <geoffrey.brenne@gmail.com>
2026-03-30 17:12:34 +0200

    FEAT : last commit before merge

--- custom_test.py
+++
@@ -1,7 +0,0 @@
-from functools import reduce
-from operator import mul
-from processional import slave
-
-if __name__ == "__main__":
-    process = slave()
-    process.invoke(lambda: print('hello from slave'))
